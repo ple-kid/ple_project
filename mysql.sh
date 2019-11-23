e02db14e689e601806fbcc724285c057847e8143
ple <pangliangen@outlook.com>
2019-11-23 10:49:50 +0800

    添加一键安装mysql脚本

---
+++ mysql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+tar -zxf mysql-5.6.35-linux-glibc2.5-x86_64.tar.gz
+mv mysql-5.6.35-linux-glibc2.5-x86_64 /usr/local/mysql
+useradd -r -s /sbin/nologin mysql
+chown -R mysql.mysql /usr/local/mysql
+cd /usr/local/mysql
+yum remove mariadb-libs -y
+scripts/mysql_install_db --user=mysql
+cp support-files/mysql.server /etc/init.d/mysql
+service mysql start
+# 追加/usr/local/mysql/bin目录到环境变量
+echo 'export PATH=$PATH:/usr/local/mysql/bin' >> /etc/profile
+source /etc/profile
